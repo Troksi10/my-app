c5e89b08fd55381d084735fa57289a432feea5fe
Ilija Trokovski <ilija.trokovski@gmail.com>
2022-09-27 02:07:06 +0200

    React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+//Create a React app from scratch.
+//Show a single h1 that says "Good morning" if between midnight and 12PM.
+//or "Good Afternoon" if between 12PM and 6PM.
+//or "Good evening" if between 6PM and midnight.
+//Apply the "heading" style in the styles.css
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
+
+import "./App.css";
+
+const date = new Date(2022, 27, 9, 19);
+const currentTime = date.getHours();
+
+let greeting;
+
+const customStyle = {
+  color: "",
+};
+
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "yellow";
+} else {
+  greeting = "Good Night";
+  customStyle.color = "blue";
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading" style={customStyle}>
+        {greeting}
+      </h1>
     </div>
   );
 }
